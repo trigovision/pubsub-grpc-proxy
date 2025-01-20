1332990319c4ad06e86c7b07fea3efb01f71f6be
Goni Zahavy <goni1993@gmail.com>
2025-01-20 09:56:33 +0200

    Update build.rs to use GOOGLE_APIS_DIR environment variable for proto compilation

--- build.rs
+++ build.rs
@@ -1,10 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let crate_root = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let google_apis = std::env::var("GOOGLE_APIS_DIR").unwrap_or(crate_root + "/proto");
+
+    
+
     tonic_build::configure()
         .build_server(true)
-        .compile_protos(
-            &["google/pubsub/v1/pubsub.proto"],
-            &[crate_root + "/proto"],
-        )?;
+        .compile_protos(&["google/pubsub/v1/pubsub.proto"], &[google_apis])?;
     Ok(())
-} 
+}
